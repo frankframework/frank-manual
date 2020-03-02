383b4fbefbe4203fc50042059e4575c60fb21b53
Martijn Dirkse <mhdirkse@live.nl>
2020-03-02 17:58:21 +0100

    TutorialStep: first step towards producing include text

--- TutorialSteps/algorithm.py
+++ TutorialSteps/algorithm.py
@@ -1,6 +1,37 @@
 comparatorIndentInsensitive = lambda first, second: first.strip() == second.strip()
 comparatorIndentSensitive = lambda first, second: first == second
 
+def getIndentAndCheck(line):
+    numIndent = 0
+    for c in line:
+        if c == " ":
+            numIndent += 1
+        elif c == "\t":
+            err = "Line contains tabs: " + line
+            return 0, err
+        elif c == "\r":
+            err = "Line contains \\r: " + line
+            return 0, err
+        elif c == "\n":
+            err = "Line contains \\n: " + line
+            return 0, err
+        else:
+            return numIndent, None
+
+def checkNonEmptyStringList(stringList):
+    if not type(stringList) is list:
+        raise TypeError("Not a list: " + str(stringList))
+    if not all([type(line) is str for line in str(stringList)]):
+        raise TypeError("List should contain only strings: " + str(stringList))
+    if len(stringList) == 0:
+        raise ValueError("String list should contain at least one line")
+
+def checkNonNegativeInt(i):
+    if not type(i) is int:
+        raise TypeError("Should be int")
+    if i < 0:
+        raise ValueError("Should not be negative")
+
 class ExpectedUpdate:
     def __init__(self, numOldLines, numNewLines):
         self._numOldLines = numOldLines
@@ -19,6 +50,98 @@ class ActualUpdate:
     def getNumNewLines(self):
         return self._expectedUpdate.getNumNewLines()
 
+class Window:
+    def __init__(self, lines, first, last):
+        checkNonEmptyStringList(lines)
+        checkNonNegativeInt(first)
+        checkNonNegativeInt(last)
+        self._lines = lines
+        self._first = first
+        self._last = last
+    def getLines(self):
+        return [self._lines[i] for i in range(self._first, self._last + 1)]
+    def widen(self, numContext):
+        checkNonNegativeInt(numContext)
+        newFirst = max(self._first - numContext, 0)
+        numPrepended = self._first - newFirst
+        self._first = newFirst
+        self._last = min(self._last + numContext, len(self._lines) - 1)
+        return numPrepended
+    def hasOverlap(self, other):
+        if not isinstance(other, Window):
+            raise TypeError("Should be Window")
+        return other._first <= self._last + 1
+    def join(self, other):
+        if not isinstance(other, Window):
+            raise TypeError("Should be Window")
+        if not self.hasOverlap(other):
+            raise ValueError("Should have overlap")
+        if not self._first <= other._first:
+            raise ValueError("When joining, the first Window comes first")
+        numPrepended = other._first - self._first
+        return Window(self._lines, self._first, other._last), numPrepended
+
+def actualUpdateToWindow(newLines, actualUpdate):
+    checkNonEmptyStringList(newLines)
+    if not isinstance(actualUpdate, ActualUpdate):
+        raise TypeError("actualUpdate should have type ActualUpdate")
+    lines = newLines
+    first = actualUpdate._firstNewLine
+    last = first + actualUpdate.getNumNewLines() - 1
+    return Window(lines, first, last)
+
+class Highlight:
+    def __init__(self, relFirst, relLast):
+        checkNonNegativeInt(relFirst)
+        checkNonNegativeInt(relLast)
+        if relFirst > relLast:
+            raise ValueError("relFirst should be <= relLast")
+        self._relFirst = relFirst
+        self._relLast = relLast
+    def shift(self, numLines):
+        checkNonNegativeInt(numLines)
+        self._relFirst += numLines
+        self._relLast += numLines
+    def getHighlights(self):
+        return range(self._relFirst, self._relLast + 1)
+
+def windowToHighlight(w):
+    return Highlight(0, w._last - w._first)
+
+class HighlightedWindow:
+    def __init__(self, w):
+        if not isinstance(w, Window):
+            raise TypeError("Should be Window")
+        self._window = w
+        self._highlights = []
+    def getLines(self):
+        return self._window.getLines()
+    def getHighlights(self):
+        result = []
+        for h in self._highlights:
+            result.extend(h.getHighlights())
+        return result
+
+    def highlightAll(self):
+        h = windowToHighlight(self._window)
+        self._highlights.append(h)
+    def widen(self, numContext):
+        numPrepended = self._window.widen(numContext)
+        for h in self._highlights:
+            h.shift(numPrepended)
+    def hasOverlap(self, other):
+        return self._window.hasOverlap(other._window)
+    def join(self, other):
+        newWindow, numPrepended = self._window.join(other._window)
+        result = HighlightedWindow(newWindow)
+        for h in self._highlights:
+            result._highlights.append(h)
+        for h in other._highlights:
+            newHighlight = Highlight(h._relFirst, h._relLast)
+            newHighlight.shift(numPrepended)
+            result._highlights.append(newHighlight)
+        return result
+
 class Comparison:
     def __init__(self, oldLines, newLines, expectedUpdates, comparator):
         self._oldLines = oldLines
@@ -29,20 +152,10 @@ class Comparison:
         self._comparisonError = None
         self.verify()
     def verify(self):
-        if not type(self._oldLines) is list:
-            raise TypeError("self._oldLines is not a list")
-        if not type(self._newLines) is list:
-            raise TypeError("self._newLines is not a list")
-        if not all([type(line) is str for line in self._oldLines]):
-            raise TypeError("self._oldLines should be a list of strings")
-        if not all([type(line) is str for line in self._newLines]):
-            raise TypeError("self._newLines should be a list of string")
-        if len(self._oldLines) == 0:
-            raise ValueError("self._oldLines should not be empty")
-        if len(self._newLines) == 0:
-            raise ValueError("self._newLines should not be empty")
+        checkNonEmptyStringList(self._oldLines)
+        checkNonEmptyStringList(self._newLines)
         if not type(self._expectedUpdates) is list:
-            raise TypeError("self._expectedUpdates should be a list, but is of type")
+            raise TypeError("self._expectedUpdates should be a list")
     def compare(self):
         currentOldLine = 0
         currentNewLine = 0
@@ -70,11 +183,12 @@ class Comparison:
         if currentNewLine != len(self._newLines):
             self._comparisonError = "Not all new lines have been explained by the expected updates"
             return
-
     def hasComparisonError(self):
         return self._comparisonError is not None
     def getComparisonError(self):
         return self._comparisonError
+    def getWindows(self):
+        return [HighlightedWindow(actualUpdateToWindow(self._newLines, actual)) for actual in self._actualUpdates]
 
 if __name__ == "__main__":
 
@@ -134,5 +248,51 @@ if __name__ == "__main__":
             comparison = Comparison(["monkey"], ["  monkey  "], [], comparatorIndentSensitive)
             comparison.compare()
             self.assertTrue(comparison.hasComparisonError())
+
+    class TestHighlightedWindow(unittest.TestCase):
+        def test_when_two_windows_touch_then_joined_correctly_first_highlight(self):
+            comparison = Comparison( \
+                ["one",                 "four", "five",        "seven"], \
+                ["one", "two", "three", "four", "five", "six", "seven"], \
+                [ExpectedUpdate(0, 2), ExpectedUpdate(0, 1)], \
+                comparatorIndentSensitive)
+            comparison.compare()
+            self.assertFalse(comparison.hasComparisonError())
+            windows = comparison.getWindows()
+            windows[0].highlightAll()
+            windows[0].widen(1)
+            windows[1].widen(1)
+            self.assertTrue(windows[0].hasOverlap(windows[1]))
+            joinWindow = windows[0].join(windows[1])
+            self.assertEquals(joinWindow.getLines(), ["one", "two", "three", "four", "five", "six", "seven"])
+            self.assertEquals([n+1 for n in joinWindow.getHighlights()], [2, 3])
+        def test_when_two_windows_touch_then_joined_correctly_second_highlight(self):
+            comparison = Comparison( \
+                ["one",        "three", "four",                "seven"], \
+                ["one", "two", "three", "four", "five", "six", "seven"], \
+                [ExpectedUpdate(0, 1), ExpectedUpdate(0, 2)], \
+                comparatorIndentSensitive)
+            comparison.compare()
+            self.assertFalse(comparison.hasComparisonError())
+            windows = comparison.getWindows()
+            windows[1].highlightAll()
+            windows[0].widen(1)
+            windows[1].widen(1)
+            self.assertTrue(windows[0].hasOverlap(windows[1]))
+            joinWindow = windows[0].join(windows[1])
+            self.assertEquals(joinWindow.getLines(), ["one", "two", "three", "four", "five", "six", "seven"])
+            self.assertEquals([n+1 for n in joinWindow.getHighlights()], [5, 6])
+        def test_when_two_windows_dont_touch_then_not_joinable(self):
+            comparison = Comparison( \
+                ["one",        "three", "four", "five",        "seven"], \
+                ["one", "two", "three", "four", "five", "six", "seven"], \
+                [ExpectedUpdate(0, 1), ExpectedUpdate(0, 1)], \
+                comparatorIndentSensitive)
+            comparison.compare()
+            self.assertFalse(comparison.hasComparisonError())
+            windows = comparison.getWindows()
+            windows[0].widen(1)
+            windows[1].widen(1)
+            self.assertFalse(windows[0].hasOverlap(windows[1]))
 
     unittest.main()
