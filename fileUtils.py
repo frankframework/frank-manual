03d54a0926f22d1b7ff7837cb06220ab34b013fc
Martijn Dirkse <mhdirkse@live.nl>
2020-03-06 15:19:57 +0100

    Add function to create directory if it does not exist

---
+++ fileUtils.py
@@ -0,0 +1,32 @@
+# Directory utils. If you run this from the command line, you test
+# creating a directory if it does not exist. You can also import
+# this file and use its makeDirectoryIfNotPresent function.
+
+import os
+
+def makeDirectoryIfNotPresent(linuxStyleRelativePath):
+    if not type(linuxStyleRelativePath) is str:
+        raise TypeError("String expected")
+    if linuxStyleRelativePath == "":
+        raise ValueError("Path should not be empty")
+    if "\\" in linuxStyleRelativePath:
+        raise ValueError("Path is not Linux-style")
+    if linuxStyleRelativePath != linuxStyleRelativePath.strip():
+        raise ValueError("Path is not stripped")
+    if linuxStyleRelativePath[0] == "/":
+        raise ValueError("Path is not relative")
+    if "." in linuxStyleRelativePath:
+        raise ValueError("Paths with . or .. not supported")
+    cmd = "mkdir -p {0}".format(linuxStyleRelativePath)
+    curPath = os.getcwd()
+    for component in linuxStyleRelativePath.split("/"):
+        curPath = os.path.join(curPath, component)
+        if not os.path.exists(curPath):
+            os.mkdir(curPath)
+
+if __name__ == "__main__":
+
+    import sys
+
+    dirToCreate = sys.argv[1]
+    makeDirectoryIfNotPresent(dirToCreate)
