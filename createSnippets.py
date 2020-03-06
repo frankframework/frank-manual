837de5b66d2528ecc68140c407b6e2ba3e5d43f2
Martijn Dirkse <mhdirkse@live.nl>
2020-03-06 17:06:35 +0100

    Put error message at end of output when checking srcSteps fails

--- createSnippets.py
+++ createSnippets.py
@@ -13,6 +13,7 @@ def handlerAddNew(comparison, relPath, lines):
 
 def createStepSnippets(configName, stepName, oldDir, newDir, snippetsDir):
     print "Doing Frank config {0} step {1}".format(configName, stepName)
+    hasErrors = False
     with newDir.openFile(META_YML) as f:
         diffs, error = expectedDifferences = TutorialSteps.createFileDifferences(f)
     if error is not None:
@@ -24,6 +25,7 @@ def createStepSnippets(configName, stepName, oldDir, newDir, snippetsDir):
     snippets, errors = compare.run()
     if errors is not None:
         for error in errors:
+            hasErrors = True
             print "ERROR: " + error
     for snippet in snippets:
         makeDirectoryIfNotPresent("/".join([configName, stepName]), os.path.abspath(snippetsDir))
@@ -31,18 +33,24 @@ def createStepSnippets(configName, stepName, oldDir, newDir, snippetsDir):
         with open(outputFileName, "w") as f:
             for line in snippet.getLines():
                 f.write(line + "\n")
+    return hasErrors
 
 def createFrankConfigSnippets(configRoot, snippetsDir):
     name = configRoot.getLastComponent()
     stepDirs = configRoot.getSubdirs()
+    hasErrors = False
     if len(stepDirs) >= 1:
-        createStepSnippets(name, stepDirs[0].getLastComponent(), None, stepDirs[0], snippetsDir)
+        hasErrors = createStepSnippets(name, stepDirs[0].getLastComponent(), None, stepDirs[0], snippetsDir)
     for stepIdx in range(1, len(stepDirs)):
         stepName = stepDirs[stepIdx].getLastComponent()
-        createStepSnippets(name, stepName, stepDirs[stepIdx-1], stepDirs[stepIdx], snippetsDir)
+        hasErrors = hasErrors or createStepSnippets(name, stepName, stepDirs[stepIdx-1], stepDirs[stepIdx], snippetsDir)
+    return hasErrors
 
 def createAllSnippets(tutorialStepsDir, snippetsDir):
     makeDirectoryIfNotPresent(snippetsDir)
     tutorialStepsRoot = TutorialSteps.GitDirectoryTree(tutorialStepsDir)
+    hasErrors = False
     for frankConfigDir in tutorialStepsRoot.getSubdirs():
-        createFrankConfigSnippets(frankConfigDir, snippetsDir)
+        hasErrors = hasErrors or createFrankConfigSnippets(frankConfigDir, snippetsDir)
+    if hasErrors:
+        print("*** ERRORS CHECKING srcSteps AND GENERATING SNIPPETS")
