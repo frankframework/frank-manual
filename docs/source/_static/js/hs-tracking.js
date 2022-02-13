4932e54aece2381d2562b4b183b579c2f163a685
Niels Lam <hoi@niels.fun>
2022-02-13 14:22:13 +0100

    Add hs tracking code

---
+++ docs/source/_static/js/hs-tracking.js
@@ -0,0 +1,7 @@
+var s = document.createElement("script");
+s.id = "hs-script-loader";
+s.type = "text/javascript";
+s.src = "//js.hs-scripts.com/8859156.js";
+s.setAttribute("defer", "defer");
+s.setAttribute("async", "async");
+document.body.appendChild(s);
