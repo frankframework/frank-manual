79e58c34c64d699e8b752aa3edaf023df50c1f53
Niels Lam <hoi@niels.fun>
2022-02-15 10:50:07 +0100

    Fix hs-script load

--- docs/source/_static/js/hs-tracking.js
+++ docs/source/_static/js/hs-tracking.js
@@ -1,7 +1,9 @@
-var s = document.createElement("script");
-s.id = "hs-script-loader";
-s.type = "text/javascript";
-s.src = "//js.hs-scripts.com/8859156.js";
-s.setAttribute("defer", "defer");
-s.setAttribute("async", "async");
-document.body.appendChild(s);
+window.onload = () => {
+    var s = document.createElement("script");
+    s.id = "hs-script-loader";
+    s.type = "text/javascript";
+    s.src = "//js.hs-scripts.com/8859156.js";
+    s.setAttribute("defer", "defer");
+    s.setAttribute("async", "async");
+    document.body.appendChild(s);
+};
