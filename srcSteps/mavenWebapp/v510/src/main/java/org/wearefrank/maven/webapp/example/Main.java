fd1f8e3a6d5ad06bd639202c1260b5c07c3ba68b
Martijn Dirkse <martijn@integrationpartners.nl>
2022-05-05 12:35:55 +0200

    Add example Maven project - still cmd

---
+++ srcSteps/mavenWebapp/v510/src/main/java/org/wearefrank/maven/webapp/example/Main.java
@@ -0,0 +1,9 @@
+package org.wearefrank.maven.webapp.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("Hello World!"));
+    }
+}
