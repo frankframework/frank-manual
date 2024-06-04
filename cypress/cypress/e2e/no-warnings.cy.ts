5e123ca045b0289bbf21b44891fa6db24c9d6fce
Martijn Dirkse <martijn@integrationpartners.nl>
2024-06-04 15:25:04 +0200

    Fix previous commit

--- cypress/cypress/e2e/no-warnings.cy.ts
+++ cypress/cypress/e2e/no-warnings.cy.ts
@@ -1,4 +1,4 @@
-describe('Tests of frank-manual config', () => {
+describe('Tests of frank-manual config - no warnings', () => {
   it('Test that there are no alerts in the status page', () => {
     cy.visit('')
     // cy.get('[data-cy-status-alert]').should('have.length', 0)
--- cypress/cypress/e2e/warning-only-iaf-util.cy.ts
+++ cypress/cypress/e2e/warning-only-iaf-util.cy.ts
@@ -1,4 +1,4 @@
-describe('Tests of frank-manual config', () => {
+describe('Tests of frank-manual config - only IAF_Util', () => {
   it('Test that the only alert in the status page is about config IAF-Util', () => {
     cy.visit('')
 	  cy.get('.alert-warning').should('have.length', 1).within(() => {
