1cec2a948ec770b8d839d1632685873e20974784
Martijn Dirkse <martijn@integrationpartners.nl>
2024-06-04 15:08:58 +0200

    Differentiate tests to allow IAF_Util warning yes/no

---
+++ .github/workflows/setExpectedBehaviorTag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export currentFrankConfig=$1
+
+export frankConfigsIafUtilAllowed=" \
+    frank-manual/srcSteps/NewHorizons/v510"
+
+# Copied from StackOverflow - checks whether ${currentFrankConfig}
+# is in ${frankConfigsIafUtilAllowed} and returns 0 in this case.
+echo ${frankConfigsIafUtilAllowed} | grep -w -q ${currentFrankConfig}
+if [ $? == 0 ]; then
+  export result="AllowIafUtilWarning"
+else
+  export result="NoWarnings"
+fi
+echo "${result}"
+echo "expectedBehaviorTag=${result}" >> $GITHUB_OUTPUT
---
+++ cypress/cypress/e2e/warning-only-iaf-util.cy.ts
@@ -0,0 +1,8 @@
+describe('Tests of frank-manual config', () => {
+  it('Test that the only alert in the status page is about config IAF-Util', () => {
+    cy.visit('')
+	  cy.get('.alert-warning').should('have.length', 1).within(() => {
+      cy.contains('Configuration [IAF_Util]').should('have.length', 1)
+    })
+  })
+})
