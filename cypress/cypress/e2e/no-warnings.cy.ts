ef708ef70d649a7f18488389bbe8100a16fe8992
Martijn Dirkse <martijn@integrationpartners.nl>
2024-08-26 17:41:59 +0200

    Restore old Cypress queries, keep the code to trigger test failure

--- cypress/cypress/e2e/no-warnings.cy.ts
+++ cypress/cypress/e2e/no-warnings.cy.ts
@@ -2,6 +2,6 @@ describe('Tests of frank-manual config - no warnings', () => {
   it('Test that there are no alerts in the status page', () => {
     cy.visit('')
     // cy.get('[data-cy-status-alert]').should('have.length', 0)
-    cy.tryToFindMoreThanZero('[data-cy-status-alert]', 10)
+    cy.tryToFindMoreThanZero('.alert-warning', 10)
   })
 })
--- cypress/cypress/e2e/warning-only-iaf-util.cy.ts
+++ cypress/cypress/e2e/warning-only-iaf-util.cy.ts
@@ -1,7 +1,7 @@
 describe('Tests of frank-manual config - only IAF_Util', () => {
   it('Test that the only alert in the status page is about config IAF-Util', () => {
     cy.visit('')
-	  cy.get('[data-cy-status-alert]').should('have.length', 1).within(() => {
+	  cy.get('.alert-warning').should('have.length', 1).within(() => {
       cy.contains('Configuration [IAF_Util]').should('have.length', 1)
     })
   })
