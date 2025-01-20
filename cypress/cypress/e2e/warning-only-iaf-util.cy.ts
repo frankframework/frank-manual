18e1d7d52b3b25927e13f473e45facb9523d8956
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-20 12:02:18 +0100

    Work in progress - extra test to omit checking frank-doc

--- cypress/cypress/e2e/warning-only-iaf-util.cy.ts
+++ cypress/cypress/e2e/warning-only-iaf-util.cy.ts
@@ -5,4 +5,18 @@ describe('Tests of frank-manual config - only IAF_Util', () => {
       cy.contains('Configuration [IAF_Util]').should('have.length', 1)
     })
   })
+
+  it('Test that only alerts are IAF-Util and FrankConfig-compatibility.xsd', () => {
+    cy.get('.alert-warning').then((alerts: JQuery<Html>) => {
+      const totalNumAlerts = alerts.length
+      cy.log(`Total number of Adapter Status alerts is: ${totalNumAlerts}`)
+      const alertsNoFrankDoc = alerts.filter((a) => {
+        const alertText = a.text()
+        const isRelevant = alertText.find('FrankConfig-compatibility') < 0
+        cy.log(`Alert with text "${alertText}" relevant: ${isRelevant}`)
+        return isRelevant
+      })
+      cy.wrap(alertsNoFrankDoc).contains('Configuration [IAF_Util]').should('have.length', 1)
+    })
+  })
 })
