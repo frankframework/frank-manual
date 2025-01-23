f45679494b77088e8e082d5d9b278a03c3f3f36c
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-23 09:43:49 +0100

    Enable tests again that also fail with FrankConfig-compatibility warnings

--- cypress/cypress/e2e/warning-only-iaf-util.cy.ts
+++ cypress/cypress/e2e/warning-only-iaf-util.cy.ts
@@ -1,6 +1,5 @@
 describe('Tests of frank-manual config - only IAF_Util', () => {
-  // Can be enabled if core fixes the warnings omitted here
-  xit('Test that the only alert in the status page is about config IAF-Util', () => {
+  it('Test that the only alert in the status page is about config IAF-Util', () => {
     cy.visit('')
 	  cy.get('.alert-warning').should('have.length', 1).within(() => {
       cy.contains('Configuration [IAF_Util]').should('have.length', 1)
