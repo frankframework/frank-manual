4a888b2ef02085fc9b8b761648dd5dba521dec5e
Martijn Dirkse <martijn@integrationpartners.nl>
2025-01-27 13:34:59 +0100

    Fix typo and disable test that detects known issue

--- cypress/cypress/e2e/config-loaded.cy.ts
+++ cypress/cypress/e2e/config-loaded.cy.ts
@@ -3,7 +3,7 @@ describe('Check that expected Frank configurations are really loaded', () => {
     cy.visit('')
     const expectedConfigs = Cypress.env('expectedFrankConfigs').split(',')
     for (const expectedConfig of expectedConfigs) {
-      cy.get(`[data-cy-select-tab=${expectedConfig}`)
+      cy.get(`[data-cy-select-tab=${expectedConfig}]`)
       // Wait to have a screenshot in the video
       cy.wait(1000)
     }
