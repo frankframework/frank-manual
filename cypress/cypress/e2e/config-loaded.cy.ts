5d3da69fdc129ed82309d9aef4a03afe690a2de4
Martijn Dirkse <martijn@integrationpartners.nl>
2025-02-13 13:46:49 +0100

    Adjust Cypress test for changing data-cy

--- cypress/cypress/e2e/config-loaded.cy.ts
+++ cypress/cypress/e2e/config-loaded.cy.ts
@@ -4,7 +4,7 @@ describe('Check that expected Frank configurations are really loaded', () => {
     // Should be passed as array already
     const expectedConfigs = Cypress.env('expectedFrankConfigs')
     for (const expectedConfig of expectedConfigs) {
-      cy.get(`[data-cy-select-tab=${expectedConfig}]`)
+      cy.get('[data-cy="tab-list__nav-tabs"]').contains(`${expectedConfig}`)
       // Wait to have a screenshot in the video
       cy.wait(1000)
     }
