4f29aef7c176aab76631788dafa8886e2a50c924
Martijn Dirkse <martijn@integrationpartners.nl>
2025-08-05 16:31:48 +0200

    Improve test

--- cypress/cypress/e2e/no-warnings.cy.ts
+++ cypress/cypress/e2e/no-warnings.cy.ts
@@ -4,11 +4,13 @@ describe('Tests of frank-manual config - no warnings', () => {
     // cy.get('[data-cy-status-alert]').should('have.length', 0)
     cy.tryToFindMoreThanZero('.alert-warning', 10)
     cy.tryToFindMoreThanZero('.alert-error', 10)
+    cy.tryToFindMoreThanZero('.alert-danger', 10)
   })
 
   it('Test that there are only alerts about FrankConfig-compatibility', () => {
     cy.visit('')
     cy.tryToFindMoreThanZeroOmitting('.alert-warning', 'FrankConfig-compatibility', 10)
     cy.tryToFindMoreThanZero('.alert-error', 10)
+    cy.tryToFindMoreThanZero('.alert-danger', 10)
   })
 })
